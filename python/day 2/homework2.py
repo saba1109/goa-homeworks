d4d564187eb194a6744b4f02d0b366e78a49fcb0
saba1109 <sabalomsadze59@gmail.com>
2024-02-08 15:12:17 +0400

    first commit

---
+++ python/day 2/homework2.py
@@ -0,0 +1,6 @@
+print("/yes")
+
+print("gavicani ori razmis lideri, razmis wevrebi.")
+
+
+print("GOA sauketeso akademiaa imis gatvaliswinebit rom aris online,sauketso mentorebi,megobruli damokidebuleba da siamovneba")
