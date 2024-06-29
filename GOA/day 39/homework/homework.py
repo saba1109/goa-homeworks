430c8fd51876c63c7b57143cff22eaefdc59ca99
saba1109 <sabalomsadze59@gmail.com>
2024-06-29 18:29:47 +0400

    commit

--- GOA/day 39/homework/homework.py
+++ GOA/day 39/homework/homework.py
@@ -1,16 +1,18 @@
 #მომხმარებელს შემოატანინეთ ათი რიცხვი, ეს რიცხვები დაამატეთ სიაში, ამ სიიდან დაახარისხეთ ეს რიცხვები ორ ჯგუფად, რიცხვები რომლებიც მეტია 100 ზე და რიცხვები რომლებიც ნაკლებია 100 ზე
 
-greater_than_100 = []
-less_than_100 = []
+numbers = []
+more = []
+less = []
+for i in range(10):
+    users_input = int(input("please enter number: "))
+    numbers.append(users_input)
 
-for _ in range(10):
-    number = input("Enter a number: ")
-
-    if number > 100:
-        greater_than_100 = greater_than_100 + [number]
+for i in range(len(numbers)):
+    if numbers[i] == 100:
+        continue
+    elif numbers[i] > 100:
+        more.append(numbers[i])
     else:
-        less_than_100 = less_than_100 + [number]
-
-
-print("Numbers greater than 100:", greater_than_100)
-print("Numbers less than or equal to 100:", less_than_100)
+        less.append(numbers[i])
+print("this numbers are more than 100: ", more)
+print("this numbers are less than 100: ", less)
