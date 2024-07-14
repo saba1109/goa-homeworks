a43c0eebbd61878569e5a468ae6958fbd8cc9c2b
saba1109 <sabalomsadze59@gmail.com>
2024-07-14 14:51:08 +0400

    commit

--- GOA/day 44/homework/forloop2.py
+++ GOA/day 44/homework/forloop2.py
@@ -1,3 +1,3 @@
 #Print the first 10 natural numbers.
-for i in range(1, 11):
+for i in range(0, 10):
     print(i)
--- GOA/day 44/homework/whileloop2.py
+++ GOA/day 44/homework/whileloop2.py
@@ -2,6 +2,7 @@
 sum = 0
 number = 1
 while number <= 10:
-    sum += number
+    sum += number   
     number += 1
+    
 print("The sum of numbers from 1 to 10 is:", sum)
